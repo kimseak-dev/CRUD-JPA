397d9add1704b78a0694e4659d61941d2598899f
kimseak dev <kimseakphon@gmail.com>
2017-05-20 15:02:08 +0700

    add lazy fetching

--- src/main/java/com/udemy/compoistionmanytomany/Professor.java
+++ src/main/java/com/udemy/compoistionmanytomany/Professor.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -23,7 +24,7 @@ public class Professor {
 	@Column(name="professor_name")
 	private String professorName;
 	
-	@ManyToMany(mappedBy="professors")
+	@ManyToMany(mappedBy="professors", fetch = FetchType.LAZY)
 	private List<ResearchProject> projects;
 	
 	public Professor(){
